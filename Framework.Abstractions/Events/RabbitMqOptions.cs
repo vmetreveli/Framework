e8bd6f3b40dcf8772633eb43e843657700b7d5ab
Vakhushti Metreveli <Vakhushti.Metreveli@gmail.com>
2024-08-22 12:18:45 +0400

    commit

---
+++ Framework.Abstractions/Events/RabbitMqOptions.cs
@@ -0,0 +1,10 @@
+namespace Framework.Abstractions.Events;
+
+public class RabbitMqOptions
+{
+    public string Host { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string ExchangeName { get; set; }
+    
+}
