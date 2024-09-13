7e271e49cb053d17778eaf709b35e81b10504517
Vakhushti Metreveli <Vakhushti.Metreveli@gmail.com>
2024-09-13 17:10:58 +0400

    added comments

--- Framework.Abstractions/Events/IEventConsumer.cs
+++ Framework.Abstractions/Events/IEventConsumer.cs
@@ -3,6 +3,12 @@ using MassTransit;
 
 namespace Framework.Abstractions.Events;
 
-public interface IEventConsumer<T> : IConsumer<T> where T :  IntegrationBaseEvent
+/// <summary>
+/// Defines a consumer for handling integration events.
+/// </summary>
+/// <typeparam name="T">The type of the integration event to be handled.</typeparam>
+public interface IEventConsumer<T> : IConsumer<T> where T : IntegrationBaseEvent
 {
+    // The IConsumer<T> interface from MassTransit provides a method to handle messages of type T.
+    // The T in this case must be an IntegrationBaseEvent, which means this consumer will handle integration events.
 }
